e6f0c1cf179e4d8669de6153c45617ffcd3f3eee
Castro-Lopez Dan <dan.castro-lopez@imerir.com>
2015-02-23 16:45:42 +0100

    Rob interface gyro
    rob+mainwindow

--- DataTeamInterface/robotinterface.cpp
+++ DataTeamInterface/robotinterface.cpp
@@ -5,8 +5,10 @@ RobotInterface::RobotInterface(Ui::MainWindow* ui)
     m_pUi = ui;
     m_pProtocole = new Protocole();
     m_pGps = new GPS(m_pProtocole);
+    m_pGyro = new Gyro(m_pProtocole);
 
      connect(m_pGps, SIGNAL(emitDataAvailable()), this, SLOT(slotOnGpsDataAvailable()));
+     connect(m_pGyro, SIGNAL(emitDataAvailable()), this, SLOT(slotOnGyroDataAvailable()));
 }
 
 RobotInterface::~RobotInterface()
@@ -16,10 +18,8 @@ RobotInterface::~RobotInterface()
 
 bool RobotInterface::connectRobot()
 {
-  if(m_pGps->ConnectCaptor(m_pUi->leServerAddress->text(),m_pUi->gpsPort->text().toInt()))
-    return true;
-  else
-      return false;
+    m_pGps->ConnectCaptor(m_pUi->leServerAddress->text(),m_pUi->gpsPort->text().toInt());
+    m_pGyro->ConnectCaptor(m_pUi->leServerAddress->text(),m_pUi->gyroPort->text().toInt());
 }
 
 void RobotInterface::slotOnGpsDataAvailable()
@@ -27,5 +27,13 @@ void RobotInterface::slotOnGpsDataAvailable()
     m_pUi->altitudeResult->setText(QString::number(m_pGps->getAltitude()));
     m_pUi->longitudeResult->setText(QString::number(m_pGps->getLongitude()));
     m_pUi->latitudeResult->setText(QString::number(m_pGps->getLatitude()));
+    m_pUi->qualityResult->setText(m_pGps->getQuality());
+}
+
+void RobotInterface::slotOnGyroDataAvailable()
+{
+    m_pUi->gyroXResult->setText(QString::number(m_pGyro->getXGyro()));
+    m_pUi->gyroYResult->setText(QString::number(m_pGyro->getYGyro()));
+    m_pUi->GyroZResult->setText(QString::number(m_pGyro->getZGyro()));
 }
 
